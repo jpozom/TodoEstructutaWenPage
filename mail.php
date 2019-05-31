db4962d286414865b5c0c73d93828b2f43ef59bf
Pozo Marambio, Johnny Alejandro (CL - Santiago) <jpozom@deloitte.com>
2019-05-31 15:33:33 -0400

    'message'

---
+++ mail.php
@@ -0,0 +1,18 @@
+<?php
+ $nombre = $_POST['name'];
+ $correo = $_POST['email'];
+ $subject = $_POST['subject'];
+ $mensaje = $_POST['message']; 
+
+
+$destinatario = "johnny.pozo.m@gmail.com";
+$asunto = "Contacto desde pagina web";
+
+$carta = "Carta: $nombre \n";
+$carta .= "Correo: $correo \n";
+$carta .= "Asunto $subject \n";
+$carta .= "Mensaje $mensaje";
+
+mail($destinatario, $asunto, $carta);
+
+?>
